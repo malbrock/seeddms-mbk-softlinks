7b67546f17487258da5cda2953d7fc750e1b8646
malbrock <info@malbrock.com>
2017-04-11 20:51:07 -0300

    Update lang.php

--- mbk-softlinks/lang.php
+++ mbk-softlinks/lang.php
@@ -1,2 +1,11 @@
 <?php
+$__lang['en_US'] = array(
+	'linked_foler' => 'Linked Folder',
+	'linked_document' => 'Linked Document'
+);
+
+$__lang['es_ES'] = array(
+	'linked_foler' => 'Carpeta Enlazada',
+	'linked_document' => 'Documento Enlazado'
+);
 ?>
